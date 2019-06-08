c8b92958a19a14a1bc37942db30d995db5b60079
mstill3 <stillwell006@gmail.com>
2019-06-08 14:59:16 -0400

    Step 6

---
+++ JackTheGiant/Assets/Scripts/Goose.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goose : MonoBehaviour
+{
+
+    public int score = 0;
+    // float num = 3.45f;
+    // string name = "matt";
+    // bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // for(int i = 0; i <= 10; i++)
+        //     Print();
+        transform.position = new Vector3(1,1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Print()
+    {
+        Debug.Log(Calculate());
+    }
+
+    int Calculate()
+    {
+        return 2;
+    }
+}
